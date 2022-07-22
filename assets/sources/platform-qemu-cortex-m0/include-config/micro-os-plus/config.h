c9804eca59933263a32c6264b688698ab240d94f
Liviu Ionescu <ilg@livius.net>
2022-07-22 13:20:56 +0300

    */config.h: MICRO_OS_PLUS_USE_SEMIHOSTING

--- assets/sources/platform-qemu-cortex-m0/include-config/micro-os-plus/config.h
+++ assets/sources/platform-qemu-cortex-m0/include-config/micro-os-plus/config.h
@@ -16,7 +16,7 @@
 // ----------------------------------------------------------------------------
 
 // On bare-metal platforms, tests are semihosted applications.
-// #define MICRO_OS_PLUS_USE_SEMIHOSTING_SYSCALLS
+#define MICRO_OS_PLUS_USE_SEMIHOSTING
 
 #if defined(MICRO_OS_PLUS_TRACE)
 #define MICRO_OS_PLUS_USE_TRACE_SEMIHOSTING_DEBUG
